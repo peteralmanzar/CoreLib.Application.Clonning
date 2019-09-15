c743401d5c2ca2f0f30a016480cbdc148920d531
Peter Almanzar <peter.almanzar@gmail.com>
2019-09-15 13:33:28 -0400

    Add project files.

---
+++ CoreLib.Application.Clonning/Cloner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CoreLib.Application.Clonning
+{
+    public static class Cloner
+    {
+        public static T Clone<T>(T source)
+        {
+            #region Guards
+            if(!typeof(T).IsSerializable)
+                throw new ArgumentException($"{nameof(source)} must be serializable.", nameof(source));
+            if(object.ReferenceEquals(source, null))
+                return default(T);
+            #endregion
+
+            IFormatter formatter = new BinaryFormatter();
+            using(var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
